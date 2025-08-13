caf4e670eea846c95a05b7c2dd3b0f7dc8e9d79b
gandromes <moto.pers2007@gmail.com>
2025-08-13 21:36:28 +0400

    second

--- includes/content.php
+++ includes/content.php
@@ -1,15 +1,17 @@
 <!--  Блок content -->
-    <div class="reviewsblock">
-        <div class="reviewheader">
-            <div class="reviewstitles">
-                <h3 class="reviewstitle">Отзывы</h3>
-                <h4 class="reviewsubtitle">Популярные отзывы наших клиентов</h4>
-            </div>
-            <div class="add-review-container">
-                <button id="addReviewBtn" class="add-review-btn">Добавить отзыв</button>
-            </div>
+<div class="reviewsblock">
+    <div class="reviewheader">
+        <div class="reviewstitles">
+            <h3 class="reviewstitle">Отзывы</h3>
+            <h4 class="reviewsubtitle">Популярные отзывы наших клиентов</h4>
         </div>
-        
-        <?php include 'includes/list.php'; ?>
-    </div>        
+        <div class="add-review-container">
+            <button id="addReviewBtn" class="add-review-btn">Добавить отзыв</button>
+        </div>
+    </div>
+    
+    <?php 
+        require_once __DIR__ . '/../public/reviews_list.php'
+    ?>
+</div>        
 <!--  Блок content -->
